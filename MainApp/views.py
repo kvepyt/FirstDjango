ca0b26c774b86bbd90800ec6da382f0cdf767a27
kvepyt <kve@kve60.ru>
2025-07-31 02:18:02 +0300

    M1 template all & errors V.2

--- MainApp/views.py
+++ MainApp/views.py
@@ -52,7 +52,7 @@ def get_item(request, item_id: int):
                 "item": item
                 }
             return render (request,"item_page.html", context)
-    return render(request, "errors.html", f'<strong>Item with id={item_id} not found </strong>')
+    return render(request, "errors.html", {'errors': [f'Item with id={item_id} not found']})
     
 
 def get_items(request):
