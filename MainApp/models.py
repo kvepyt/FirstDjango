f744bcbc588c62418832f09a328fb9a8b1c69980
kvepyt <kve@kve60.ru>
2025-07-31 21:59:52 +0300

    M3 Add Item model and make migration

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    count = models.PositiveIntegerField()
+#    description = models.TextField(max_length=300, default="Описание элемента")
+#    colors = models.ManyToManyField(to=Color)
+
+#    def __repr__(self):
+#        return f'Item({self.name})'
