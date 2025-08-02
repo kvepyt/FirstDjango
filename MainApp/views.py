1a2f5acd2d1b35c590f09df5af1f2e9471e9b68c
kvepyt <kve@kve60.ru>
2025-08-02 05:23:20 +0300

    M4 part 2 task 1-2. Done

--- MainApp/views.py
+++ MainApp/views.py
@@ -38,8 +38,10 @@ def get_item(request, item_id: int):
     except ObjectDoesNotExist:
         return render(request, "errors.html", {'errors': [f'Item with id={item_id} not found']})
     else:
+        colors = item.colors.all()
         context = {
             "item": item,
+            "colors": colors,
         }
     return render(request, "item_page.html", context)
 
